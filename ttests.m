af1f887c4cb77a90caef4d4db95f8d6fcd9e721f
Giovanni Charles <giovanni.charles@gmail.com>
2012-12-06 21:37:07 +0000

    hyps

--- ttests.m
+++ ttests.m
@@ -1,19 +1,17 @@
-f1s = zeros(3,6,10);
-
-%load f1s
-
 alpha = 0.05/3;
 
+hyps = {};
 hyp = zeros(3);
 
 for emotion = 1:6
     for structure_1 = 1:3
         for structure_2 = structure_1 + 1:3
-            x = f1s(structure_1,emotion,:);
-            y = f1s(structure_2,emotion,:);
+            x = algorithms{structure_1}.cs(emotion,:);
+            y = algorithms{structure_2}.cs(emotion,:);
             h = ttest(x, y, alpha);
             hyp(structure_1, structure_2) = h;
             hyp(structure_2, structure_1) = h;
         end
     end
+    hyps{emotion} = hyp;
 end
